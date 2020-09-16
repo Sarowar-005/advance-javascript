18cb2509404402d4b56d540126860b9844739268
Sarowar-005 <sarowarhosenakib2@gmail.com>
2020-09-17 01:42:15 +0600

    update arguments.js

--- arguments.js
+++ arguments.js
@@ -1,5 +1,11 @@
+var sum = 0;
 function add(num1, num2){
-    return num1 + num2;
+    const args = [...arguments]
+    for (let i = 0; i < args.length; i++) {
+        const element = args[i];
+        sum = sum + element;
+    }
+    return sum;
 }
-const result = add(10,20);
+const result = add(10,20,40);
 console.log(result);
